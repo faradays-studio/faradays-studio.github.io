840b3d81626bda53a794e6396b86024c842a395f
faradays-studio <yvany@qq.com>
2020-09-07 00:09:01 +0800

    Updated by Faradays' Studio: 2020-09-07 00:08:59

---
+++ downloads/code/test.bat
@@ -0,0 +1,14 @@
+:: test.bat （需要保存为 ANSI 编码后双击运行）
+@echo off
+echo 当前路径的完整字符串：%0
+echo 去掉引号：%~0
+echo 所在分区：%~d0
+echo 所处路径：%~p0
+echo 文件名：%~n0
+echo 扩展名：%~x0
+echo 文件属性：%~a0
+echo 修改时间：%~t0
+echo 文件大小：%~z0
+
+echo.
+pause
